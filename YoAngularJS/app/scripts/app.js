9c9c2daa0b4d0693457f58548c9f2a726e18227c
Gavin-Steele <gavinsteele@mac.com>
2013-09-20 20:58:14 +0100

    Adding AngularJS and testing ASANA

---
+++ YoAngularJS/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('YoAngularJSApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
